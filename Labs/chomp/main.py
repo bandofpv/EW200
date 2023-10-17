cb585ea19ea3a3e54fc2dd18429ee4dc3812bd07
Andrew Bernas <andrewrbernas@gmail.com>
2023-10-17 08:40:00 -0400

    Completed Lab 7

--- Labs/chomp/main.py
+++ Labs/chomp/main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+WIDTH = 500
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+running = True
+
+background = pygame.Surface((WIDTH, HEIGHT))
+background.fill((66, 35, 223))
+pygame.draw.rect(background, (215,233,123), (0,WIDTH-100, WIDTH, 100))
+screen.blit(background, (0,0))
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+pygame.quit()
