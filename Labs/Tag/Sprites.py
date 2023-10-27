c53482920cd76a2f91dc5c737f6eb4c10be9be58
Andrew Bernas <andrewrbernas@gmail.com>
2023-10-26 22:15:54 -0400

    Added gravity and playable sprites

--- Labs/Tag/Sprites.py
+++ Labs/Tag/Sprites.py
@@ -3,15 +3,54 @@ import pygame
 
 # define a class called Player
 class Player(pygame.sprite.Sprite):
-    def __init__(self, screen, x, y):
+    def __init__(self, x, y):
         super().__init__()
         self.image = pygame.Surface((25,25))
         self.image.fill((0,255,0))
-        self.screen = screen
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
-        self.velocity = 1
+        self.velocity = 3
+        self.xvelocity = 0
+        self.yvelocity = 0
+
+    def update(self, platforms):
+        key = pygame.key.get_pressed()
+        ground = self.collision(0, 1, platforms)
+        self.xvelocity = 0
+        if key[pygame.K_LEFT]:
+            self.xvelocity = -self.velocity
+        if key[pygame.K_RIGHT]:
+            self.xvelocity = self.velocity
+        if key[pygame.K_UP] and ground:
+            self.yvelocity = -20
+        elif self.yvelocity < 9.81 and not ground:
+            self.yvelocity += 1
+        elif ground:
+            self.yvelocity = 0
+        self.move(self.xvelocity, self.yvelocity)
+
+    def collision(self, x, y, platforms):
+        self.rect.move_ip([x, y])
+        collide = pygame.sprite.spritecollideany(self, platforms)
+        self.rect.move_ip([-x, -y])
+        return collide
+
+    def move(self, x, y):
+        self.rect.move_ip(x, y)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((25,25))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
     def update(self):
-        self.rect.y += self.velocity
+        pass
+
     def draw(self, screen):
         screen.blit(self.image, self.rect)
--- Labs/Tag/main.py
+++ Labs/Tag/main.py
@@ -3,20 +3,26 @@ from Sprites import *
 
 # pygame setup
 pygame.init()
+clock = pygame.time.Clock()
+
 WIDTH = 1080  # screen width
 HEIGHT = 608  # screen height
 screen = pygame.display.set_mode((WIDTH,HEIGHT))  # create screen object
+screen.fill((66, 35, 223))  # fill the surface with blue
 
-background = pygame.Surface((WIDTH, HEIGHT))  # create surface object
-background.fill((66, 35, 223))  # fill the surface with blue
-screen.blit(background, (0, 0))
 
-player = pygame.sprite.Group()
-player.add(Player(screen, 100, 100))
+player = Player(100, 400)
+
+platforms = pygame.sprite.Group()
+for x in range(0, WIDTH, 25):
+    platforms.add(Platform(x, 500))
 
 running = True
 
 while running:
+
+    player.update(platforms)
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -24,9 +30,12 @@ while running:
             running = False
 
     # RENDER YOUR GAME HERE
-    player.update()
+    screen.fill((66, 35, 223))
     player.draw(screen)
+    platforms.draw(screen)
+
     # flip() the display to put your work on screen
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
