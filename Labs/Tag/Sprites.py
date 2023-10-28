577319f84ae5c6f62ffec70634aadb82eae2a4d8
Andrew Bernas <andrewrbernas@gmail.com>
2023-10-27 21:25:59 -0400

    Added top, bottom, right, and left collision.

--- Labs/Tag/Sprites.py
+++ Labs/Tag/Sprites.py
@@ -3,35 +3,57 @@ import pygame
 
 # define a class called Player
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, color, keys):
         super().__init__()
-        self.image = pygame.Surface((25,25))
-        self.image.fill((0,255,0))
+        self.image = pygame.Surface((25, 25))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.velocity = 3
         self.xvelocity = 0
         self.yvelocity = 0
+        self.right_key = keys[0]
+        self.left_key = keys[1]
+        self.up_key = keys[2]
+        self.jumpvelocity = -15
+        self.fallvelocity = 10
 
     def update(self, platforms):
-        key = pygame.key.get_pressed()
-        ground = self.collision(0, 1, platforms)
+        top_collide = self.touch(0, self.yvelocity, platforms)
+        bottom_collide = self.touch(0, self.fallvelocity, platforms)
+        right_collide = self.touch(self.velocity, 0, platforms)
+        left_collide = self.touch(-self.velocity, 0, platforms)
         self.xvelocity = 0
-        if key[pygame.K_LEFT]:
-            self.xvelocity = -self.velocity
-        if key[pygame.K_RIGHT]:
-            self.xvelocity = self.velocity
-        if key[pygame.K_UP] and ground:
-            self.yvelocity = -20
-        elif self.yvelocity < 9.81 and not ground:
+        key = pygame.key.get_pressed()
+        if key[self.left_key]:
+            if left_collide and self.yvelocity == 0:
+                self.rect.left = left_collide[0].rect.right
+            elif left_collide:
+                self.xvelocity = 0
+            else:
+                self.xvelocity = -self.velocity
+        if key[self.right_key]:
+            if right_collide and self.yvelocity == 0:
+                self.rect.right = right_collide[0].rect.left
+            elif right_collide:
+                self.xvelocity = 0
+            else:
+                self.xvelocity = self.velocity
+        if key[self.up_key] and self.yvelocity == 0 and bottom_collide:
+            self.yvelocity = self.jumpvelocity
+        elif self.yvelocity < self.fallvelocity and not bottom_collide:
             self.yvelocity += 1
-        elif ground:
+        if self.yvelocity > 0 and bottom_collide:
+            self.rect.bottom = bottom_collide[0].rect.top
+            self.yvelocity = 0
+        if self.yvelocity < 0 and top_collide:
+            self.rect.top = top_collide[0].rect.bottom
             self.yvelocity = 0
         self.move(self.xvelocity, self.yvelocity)
 
-    def collision(self, x, y, platforms):
+    def touch(self, x, y, platforms):
         self.rect.move_ip([x, y])
-        collide = pygame.sprite.spritecollideany(self, platforms)
+        collide = pygame.sprite.spritecollide(self, platforms, False)
         self.rect.move_ip([-x, -y])
         return collide
 
@@ -41,11 +63,12 @@ class Player(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
+
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((25,25))
-        self.image.fill((255,0,0))
+        self.image = pygame.Surface((25, 25))
+        self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
--- Labs/Tag/main.py
+++ Labs/Tag/main.py
@@ -8,20 +8,29 @@ clock = pygame.time.Clock()
 WIDTH = 1080  # screen width
 HEIGHT = 608  # screen height
 screen = pygame.display.set_mode((WIDTH,HEIGHT))  # create screen object
-screen.fill((66, 35, 223))  # fill the surface with blue
+screen.fill('black')  # fill the surface with blue
 
-
-player = Player(100, 400)
+arrow_keys = (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP)
+wasd_keys = (pygame.K_d, pygame.K_a, pygame.K_w)
+player1 = Player(100, 400, 'green', arrow_keys)
+player2 = Player(200, 400, 'blue', wasd_keys)
 
 platforms = pygame.sprite.Group()
 for x in range(0, WIDTH, 25):
     platforms.add(Platform(x, 500))
 
+for x in range(400, 600, 25):
+    platforms.add(Platform(x, 400))
+
+for x in range(700, 900, 25):
+    platforms.add(Platform(x, 475))
+
 running = True
 
 while running:
 
-    player.update(platforms)
+    player1.update(platforms)
+    player2.update(platforms)
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -30,8 +39,9 @@ while running:
             running = False
 
     # RENDER YOUR GAME HERE
-    screen.fill((66, 35, 223))
-    player.draw(screen)
+    screen.fill('black')
+    player1.draw(screen)
+    player2.draw(screen)
     platforms.draw(screen)
 
     # flip() the display to put your work on screen
