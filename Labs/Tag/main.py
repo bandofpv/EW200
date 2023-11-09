0a8bae89fa16f5cbe447e4ae1c0b1891a1a1f5a8
Andrew Bernas <andrewrbernas@gmail.com>
2023-11-08 21:58:40 -0500

    Added 12 Weeks Prep and added graphics, tag collision, and freeze to the game.

--- Labs/Tag/main.py
+++ Labs/Tag/main.py
@@ -12,25 +12,42 @@ screen.fill('black')  # fill the surface with blue
 
 arrow_keys = (pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP)
 wasd_keys = (pygame.K_d, pygame.K_a, pygame.K_w)
-player1 = Player(100, 400, 'green', arrow_keys)
-player2 = Player(200, 400, 'blue', wasd_keys)
+player1 = Player(100, 400, 'green', arrow_keys, True)
+player2 = Player(200, 400, 'blue', wasd_keys, False)
+
+
+def build_platform(x, y, length):
+    # TODO add if for when length too long
+    if x + length > WIDTH:
+        length = length - ((x + length) - WIDTH)
+    for i in range(x+25, x+length-25, 25):
+        platforms.add(Platform(i, y, 'center'))
+    platforms.add(Platform(x, y, 'left'))
+    platforms.add(Platform(x+length-25, y, 'right'))
+
 
 platforms = pygame.sprite.Group()
-for x in range(0, WIDTH, 25):
-    platforms.add(Platform(x, 500))
 
-for x in range(400, 600, 25):
-    platforms.add(Platform(x, 400))
+build_platform(0, 500, WIDTH)
+build_platform(400, 400, 200)
+build_platform(200, 300, 200)
+build_platform(700, 200, 800)
 
-for x in range(700, 900, 25):
-    platforms.add(Platform(x, 475))
+# for x in range(400, 600, 25):
+#     platforms.add(Platform(x, 400))
+#
+# for x in range(200, 400, 25):
+#     platforms.add(Platform(x, 300))
+#
+# for x in range(700, 900, 25):
+#     platforms.add(Platform(x, 475))
 
 running = True
 
 while running:
 
-    player1.update(platforms)
-    player2.update(platforms)
+    player1.update(platforms, player2)
+    player2.update(platforms, player1)
 
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
--- Labs/Tag/sprites.py
+++ Labs/Tag/sprites.py
@@ -3,12 +3,22 @@ import pygame
 
 # define a class called Player
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, color, keys):
+    def __init__(self, x, y, color, keys, it):
         super().__init__()
-        self.image = pygame.Surface((25, 25))
-        self.image.fill(color)
+        self.color = color
+        self.it = it
+        self.it_body = pygame.image.load('assets/images/red_body.png')
+        self.it_body = pygame.transform.smoothscale(self.it_body, [25, 25])
+        self.body = pygame.image.load(f'assets/images/{self.color}_body.png')
+        self.body = pygame.transform.smoothscale(self.body, [25, 25])
+        self.image = self.body
+        self.pause = False
+        if self.it:
+            self.image = self.it_body
+            self.pause = True
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.timer = pygame.time.get_ticks()
         self.velocity = 3
         self.xvelocity = 0
         self.yvelocity = 0
@@ -18,59 +28,69 @@ class Player(pygame.sprite.Sprite):
         self.jumpvelocity = -15
         self.fallvelocity = 10
 
-    def update(self, platforms):
+    def update(self, platforms, opponent):
         top_collide = self.touch(0, self.yvelocity, platforms)
         bottom_collide = self.touch(0, self.fallvelocity, platforms)
         right_collide = self.touch(self.velocity, 0, platforms)
         left_collide = self.touch(-self.velocity, 0, platforms)
+        opponent_collide = pygame.Rect.colliderect(self.rect, opponent.rect)
+        update_time = pygame.time.get_ticks()
+        if opponent_collide and not self.it and (update_time - self.timer) > 3000:
+            self.image = self.it_body
+            self.it = True
+            self.pause = True
+            self.timer = update_time
+        if opponent_collide and self.it and (update_time - self.timer) > 3000:
+            self.image = self.body
+            self.it = False
+            self.pause = False
+            self.timer = update_time
+        if (update_time - self.timer) > 3000:
+            self.pause = False
         self.xvelocity = 0
         key = pygame.key.get_pressed()
         if key[self.left_key]:
             if left_collide and self.yvelocity == 0:
-                self.rect.left = left_collide[0].rect.right
-            elif left_collide:
-                self.xvelocity = 0
-            else:
+                self.rect.left = left_collide.rect.right
+            elif not left_collide:
                 self.xvelocity = -self.velocity
         if key[self.right_key]:
             if right_collide and self.yvelocity == 0:
-                self.rect.right = right_collide[0].rect.left
-            elif right_collide:
-                self.xvelocity = 0
-            else:
+                self.rect.right = right_collide.rect.left
+            elif not right_collide:
                 self.xvelocity = self.velocity
         if key[self.up_key] and self.yvelocity == 0 and bottom_collide:
             self.yvelocity = self.jumpvelocity
         elif self.yvelocity < self.fallvelocity and not bottom_collide:
             self.yvelocity += 1
         if self.yvelocity > 0 and bottom_collide:
-            self.rect.bottom = bottom_collide[0].rect.top
+            self.rect.bottom = bottom_collide.rect.top
             self.yvelocity = 0
         if self.yvelocity < 0 and top_collide:
-            self.rect.top = top_collide[0].rect.bottom
+            self.rect.top = top_collide.rect.bottom
             self.yvelocity = 0
-        self.move(self.xvelocity, self.yvelocity)
+        if self.pause:
+            self.xvelocity = 0
+            self.yvelocity = 0
+        self.rect.move_ip(self.xvelocity, self.yvelocity)
 
     def touch(self, x, y, platforms):
         self.rect.move_ip([x, y])
-        collide = pygame.sprite.spritecollide(self, platforms, False)
+        collide = pygame.sprite.spritecollideany(self, platforms)
         self.rect.move_ip([-x, -y])
         return collide
-
-    def move(self, x, y):
-        self.rect.move_ip(x, y)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, side):
         super().__init__()
-        self.image = pygame.Surface((25, 25))
-        self.image.fill((255, 0, 0))
+        self.image = pygame.image.load(f'assets/images/tile_{side}.png')
+        self.image = pygame.transform.smoothscale(self.image, [25,25])
         self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
+        self.rect.topleft = [x, y]
 
     def update(self):
         pass
